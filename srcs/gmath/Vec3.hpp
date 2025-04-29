61e827835d806032e22bebba118c4f94cf33ee66
Gugus <gugus.deroubaix@gmail.com>
2025-04-29 13:57:51 +0200

    patch cross product

--- srcs/gmath/Vec3.hpp
+++ srcs/gmath/Vec3.hpp
@@ -377,11 +377,9 @@ namespace gm {
 	template <typename T>
 	Vec3<T>	cross(const Vec3<T> &v1, const Vec3<T> &v2)
 	{
-		T	x = (v1.y * v2.z) - (v1.z * v2.y);
-		T	y = (v1.z * v2.x) - (v1.x * v2.z);
-		T	z = (v1.x * v2.y) - (v1.y * v2.z);
-
-		return (Vec3<T>(x, y, z));
+		return (Vec3<T>((v1.y * v2.z) - (v1.z * v2.y),
+						(v1.z * v2.x) - (v1.x * v2.z),
+						(v1.x * v2.y) - (v1.y * v2.x)));
 	}
 
 	//**** STATIC FUNCTIONS ****************************************************
